93b7be8151a63b1c5e017f1156e7dd700e1098e7
hasanzade7832 <hasanzade.hoseyn27@gmail.com>
2025-07-29 21:08:56 +0330

    added tailwind style in blazor

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+﻿module.exports = {
+    content: [
+        "./Pages/**/*.{razor,cshtml,html}",
+        "./Components/**/*.{razor,cshtml,html}",
+        "./wwwroot/index.html"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
