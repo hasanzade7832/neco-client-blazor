07cb36d04bae779684aa911b8be3da2a1ad3ce0a
hasanzade7832 <hasanzade.hoseyn27@gmail.com>
2025-08-01 15:54:04 +0330

    REFACTORED task view

--- Models/MenuItemModel.cs
+++ Models/MenuItemModel.cs
@@ -1,3 +1,5 @@
 ﻿namespace BlazorApp12.Models;
 
-public record MenuItemModel(string Icon, string Text);
+public record MenuItemModel(string Icon, string Text, bool HasSubmenu);
+
+//این برای سلکت آپشن ها است 
---
+++ Models/PropertyItemModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorApp12.Models;
+
+public class PropertyItemModel
+{
+    public string Text { get; set; } = "";
+    public bool Checked { get; set; }
+}
